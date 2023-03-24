98bdda0c286a510d7fa8e8df2b67f9dc5fb25e32
sanyuktaberad <sanyukta.berad@gmail.com>
2023-03-24 21:56:35 +0530

    Refactor: Code for player rolles the die is added.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,6 +3,12 @@ package com.bridgelabz;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Player is at 0th Position.");
+		
+		int max = 6;
+		int min = 1;
+		int diceRoll = (int) (Math.random()*(max-min+1)+min); 
+		
+		System.out.println("After rolling the dice the number we get is "+ diceRoll);
 	}
 
 }
