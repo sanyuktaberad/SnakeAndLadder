e0f148c82c4ed14c7cb26d32712ed72a91e75023
sanyuktaberad <sanyukta.berad@gmail.com>
2023-03-24 22:20:12 +0530

    Refactor: code for no play snake or ladder is added.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -9,6 +9,24 @@ public class SnakeAndLadder {
 		int diceRoll = (int) (Math.random()*(max-min+1)+min); 
 		
 		System.out.println("After rolling the dice the number we get is "+ diceRoll);
+		
+		System.out.println("Dice Rolled: " + diceRoll);
+        int stepCount = 0;
+        int options = (int) (Math.random() *( 3 - 1 + 1)+1);
+        System.out.println("option is " + options);
+
+        if (options == 1) {
+            System.out.println("No Play");
+            stepCount = stepCount;
+        } else if (options == 2) {
+            System.out.println("There is Ladder");
+            stepCount = stepCount + diceRoll;
+        } else if (options == 3) {
+            System.out.println("There is Snake");
+            stepCount = stepCount - diceRoll;
+        } else {
+            System.out.println("Roll dice again");
+        }
 	}
 
 }
